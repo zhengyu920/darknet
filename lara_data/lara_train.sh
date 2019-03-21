60d0c69f2230938d1bcd266899aa10feacdccb99
Yu Zheng <zhengyu920@gmail.com>
2019-03-21 02:06:11 -0700

    added lara

---
+++ lara_data/lara_train.sh
@@ -0,0 +1,3 @@
+./darknet detector train lara_data/lara.data lara_data/yolov2_lara.cfg darknet19_448.conv.23
+
+./darknet detect lara_data/yolov2_lara.cfg /media/yu/Data/lara_backup/yolov2_lara_10000.weights lara_data/test_image.jpg 
